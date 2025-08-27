44473caf3a29f2cebea3e218dcacba6194591f46
srullsim <simchonisrael@gmail.com>
2025-08-27 16:24:56 +0300

    added producer and consumer

--- kafka/consumer-1.py
+++ kafka/consumer-1.py
@@ -2,30 +2,17 @@ import os
 import json
 from kafka import KafkaConsumer
 import datetime
-from dal-mongo import MongoDAL
-
-
-
-
-
-HOST = os.getenv("HOST", "localhost")
-USER = os.getenv("USER", None)
-PASSWORD = os.getenv("PASSWORD", None)
-DB = os.getenv("DATABASE", "news_reports")
-COLLECTION = os.getenv("COLLECTION", "interesting_news")
 
 
 class Consumer:
 
-
-
     def __init__(self, topic):
         self.topic = topic
         self.consumer= self.get_consumer_events()
-        self.dal = DAL_Mongo(HOST, DB, COLLECTION)
+        self.event = None
 
     def get_consumer_events(self):
-
+        """ create consumer object """
         consumer = KafkaConsumer(
             self.topic,
             bootstrap_servers='localhost:9092',
@@ -35,18 +22,4 @@ class Consumer:
         )
         return consumer
 
-    def consumer_with_auto_commit(self):
-        events = self.get_consumer_events()
-        # self.send_to_db(events)
-        try:
-            self.print_messages(events)
-        except Exception as e:
-            print(f"Error while consuming messages: {e}")
-            return {"Error : ": e}
-        finally:
-            events.close()
-            print("event closed")
 
-    def print_messages(self, events):
-        for message in events:
-            print(f"{message.topic}:{message.partition}:{message.offset}: key={message.key} value={message.value}")
