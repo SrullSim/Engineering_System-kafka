5c82d92e46fe50d7263bee1f00a2994584cb9e8d
asher <aw0534145493@gmail.com>
2025-08-27 16:28:26 +0300

    test

--- dal-mongo/dal.py
+++ dal-mongo/dal.py
@@ -2,16 +2,17 @@ from pymongo import MongoClient
 
 
 class MongoDAL:
-    def __init__(self, host="localhost", port=27017, db_name=None, collection_name=None):
-        self.client = MongoClient(host, port)
+    def __init__(self,uri , db_name, collection_name):
+        self.client = MongoClient(uri)
         self.db = self.client[db_name]
         self.collection = self.db[collection_name]
 
     def get_collection(self):
 
-        return list(self.collection.find({ }, {"_id": 0}))
+        return list(self.collection.find({ }))
 
     def add_document(self, message):
         self.collection.insert_one(message)
         return {"status": "success", "message": "added"}
+
 
---
+++ processes/retriver/loader.py
@@ -0,0 +1,4 @@
+from
+class Loader:
+    def __init__(self,uri , db_name, collection_name)
+        self.db =
