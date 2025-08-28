4ab490481b00d1d7142f1fa5c19861713c9fb0f9
srullsim <simchonisrael@gmail.com>
2025-08-28 09:45:14 +0300

    cleaner preprocessor done

--- processes/preprocessor/cleaner.py
+++ processes/preprocessor/cleaner.py
@@ -3,12 +3,15 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
 
 
 class Cleaner:
 
     def __init__(self):
         self.text = None
+
 
     @staticmethod
     def remove_punctuation_marks(text):
@@ -23,20 +26,38 @@ class Cleaner:
         clean_text = text.translate(str.maketrans('', '', string.punctuation))
         return clean_text
 
+
     @staticmethod
     def remove_Remove_whitespace(text):
         clean_text = re.sub(r'\s+', ' ', text).strip()
         return clean_text
+
+
+    @staticmethod
+    def remove_stop_words(text):
+        nltk.download('stopwords', quiet=True)
+        nltk.download('punkt_tab', quiet=True)
+        stop_words = set(stopwords.words('english'))
+        tokens = word_tokenize(text.lower())
+        filtered_tokens = [word for word in tokens if word not in stop_words]
+
+        return  " ".join(filtered_tokens)
+
+
+    @staticmethod
+    def find_the_roots_of_words(text):
+        ps = PorterStemmer()
+        words = word_tokenize(text)
+        stemmed_words = [ps.stem(word) for word in words]
+        return ' '.join(stemmed_words)
+
 
     @staticmethod
     def uppercase_to_lowercase(text):
         return text.lower()
 
 
-    @staticmethod
-    def remove_stop_words(text):
-        pass
-
-
-# test_text = " self. the #    # m  and with for about  kdir & git % in @ OPen? "
-# c = Cleaner
+test_text = (" self. the #    # m  and with for about  kdir & git % in @ OPen?"
+             "Programmers program with programming languages ")
+c = Cleaner
+print(c.find_the_roots_of_words(test_text))
--- processes/preprocessor/manager_cleaner.py
+++ processes/preprocessor/manager_cleaner.py
@@ -1,6 +1,6 @@
-from producer import Producer
-from consumer import Consumer
-
+from cleaner import Cleaner
+from kafka_files.producer import Producer
+from kafka_files.consumer import Consumer
 
 class ManagerKafka:
 
@@ -9,10 +9,9 @@ class ManagerKafka:
         self.producer = Producer()
         self.consumer = Consumer(self.topic)
         self.event = self.consumer.get_consumer_events()
-
+        self.cleaner = Cleaner()
 
     def consume_and_produce(self):
-        for massage in self.event:
-            pass
-
+        for massage in self.event.value():
+            self.cleaner.remove_punctuation_marks()
 
